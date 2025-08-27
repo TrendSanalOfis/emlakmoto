5dd8b1f7fe98d9248209b2065b7575f69cf16ebd
TrendSanalOfis <trendsanalofis@gmail.com>
2025-08-27 16:25:51 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Emlak ve Oto Sitesi</h1>
+      <p>Hoş geldiniz!</p>
+    </div>
+  );
+}
+
+export default App;
